0f5d9255b17f4945cc71a2dc263cd7cf43144c39
zhendongchen <zhendongchen@emotibot.com>
2023-04-26 19:07:30 +0800

    base

---
+++ base.py
@@ -0,0 +1,18 @@
+
+
+# --------------
+str ="abc"+ \
+     "----csc"
+print(str)
+str="""
+  多行文本
+""" 
+#--------------
+int_a=9
+tmp=None;
+if int_a==1:
+  tmp="if";
+elif int_a==3:
+  tmp = "elif"
+else:
+  tmp = "else" 
