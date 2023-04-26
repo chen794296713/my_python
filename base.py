d96b5565247b025bf37748a18e14f803fb7ff903
zhendongchen <zhendongchen@emotibot.com>
2023-04-26 19:46:29 +0800

    a

--- base.py
+++ base.py
@@ -1,18 +1,26 @@
 
 
-# --------------
-str ="abc"+ \
-     "----csc"
-print(str)
-str="""
-  多行文本
-""" 
-#--------------
-int_a=9
-tmp=None;
-if int_a==1:
-  tmp="if";
-elif int_a==3:
-  tmp = "elif"
-else:
-  tmp = "else" 
+#---------------- 元组 只读列表
+# tuple = ('runoob', 786, 2.23, 'john', 70.2)
+# print(tuple[0:2])
+#--------------- list
+# list = ['runoob', 786, 2.23, 'john', 70.2]
+# list[1]=777
+# print(list)
+# print(list[0:2])
+# print(list[0:2])
+#---------------- 字符串
+# * 执行多次 []  --->[)
+# str_a="abcdef"
+# print(str_a[0:5:2])
+# print(str_a[0:1]*2)
+# print(str_a[-3:-1])
+
+# a=b=c=19
+# a ,b ,c = 19,10,30
+# # --------------
+# str ="abc"+ \
+#      "----csc"
+# str="""
+#   多行文本
+# """
