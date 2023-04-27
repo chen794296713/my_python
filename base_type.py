cd0e0f372b52bd4841b1dceccf24df26d8a67492
zhendongchen <zhendongchen@emotibot.com>
2023-04-27 16:43:59 +0800

    s

---
+++ base_type.py
@@ -0,0 +1,37 @@
+
+# list = [1,2,34,5]
+# list1 = [1, 2, 34, 5]
+# # list.append(6)
+# # del list[2]
+# list2= list+list1
+# print(list2)
+# str = "hello %s"%("kkk")
+# print(str)
+# -------- number
+# import math
+# int_a =10
+# dir(math)
+# print(abs(132456.111))
+
+
+# def init(name,age):
+#   print(name,age)
+# 必备参数
+# init("ll",123)
+# # 关键字参数
+# init(age=777,name="lili")
+# 默认参数
+# def init2(name, age=111):
+#   print(name, age)
+# init2("lll")
+#  不定长参数
+# def init3(name, *values):
+#   print(name)
+#   for value in values:
+#     print(value)
+# init3("cc","111",999,[1])
+
+# add=lambda a,b:a+b
+# print(add(1,2))
+# print(add([1],[2]))
+# print(add("123","444"))
